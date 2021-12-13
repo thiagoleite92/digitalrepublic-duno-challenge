f6337403447728908c4edb45162c05528959c3ce
Thiago Leite <leite.tjs@gmail.com>
2021-12-13 15:54:57 -0300

    refactoring cpf middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv')
 
 const PORT = 3000;
 
--- middlewares/User.js
+++ middlewares/User.js
@@ -1,17 +1,19 @@
 const BAD_STATUS = 400;
 const NAME_MIN_LENGTH = 12;
-const CPF_PATTERN = new RegExp(/^[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}/)
+const CPF_MAX_LENGTH = 11;
+const CPF_PATTERN = new RegExp(/^[0-9]{3}[0-9]{3}[0-9]{3}[0-9]{2}/)
 
 const isValidName = (req, res, next) => {
   const { name } = req.body;
+
+  if (!name) {
+    return res.status(BAD_STATUS).json('Name must be informed');
+  }
 
   if (typeof name !== 'string') {
     return res.status(BAD_STATUS).json('Name must be letters');
   }
 
-  if (!name) {
-    return res.status(BAD_STATUS).json('Name must be informed');
-  }
 
   if (name.length < NAME_MIN_LENGTH) {
     return res.status(BAD_STATUS).json('Name must be at least ' + NAME_MIN_LENGTH)
@@ -23,16 +25,16 @@ const isValidName = (req, res, next) => {
 const isValidCpf = (req, res, next) => {
   const { cpf } = req.body;
 
-  if (typeof cpf !== 'string') {
-    return res.status(BAD_STATUS).json('Cpf must be letters');
+  if (!cpf) {
+    return res.status(BAD_STATUS).json('CPF must be informed');
   }
 
-  if (!cpf) {
-    return res.status(BAD_STATUS).json('CPF must be informed')
+  if (typeof cpf !== 'string' || cpf.length > CPF_MAX_LENGTH) {
+    return res.status(BAD_STATUS).json('Cpf: only letters are needed');
   }
 
   if (!cpf.match(CPF_PATTERN)) {
-    return res.status(BAD_STATUS).json('CPF informed is not valid')
+    return res.status(BAD_STATUS).json('CPF informed is not valid');
   }
 
   next();
